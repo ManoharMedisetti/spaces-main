8734d4329f698e979e30a158a4ab0b9f670a4afd
Abhishek26062002 <abhishekpeddada26@gmail.com>
2025-07-02 16:04:41 +0530

    Update Sidebar.jsx

--- project/src/components/Sidebar.jsx
+++ project/src/components/Sidebar.jsx
@@ -45,7 +45,7 @@ export default function Sidebar() {
           <div className="w-8 h-8 bg-violet-600 rounded-lg flex items-center justify-center">
             <BookOpen className="w-5 h-5 text-white" />
           </div>
-          <span className="font-bold text-lg tracking-tight">TutorWise</span>
+          <span className="font-bold text-lg tracking-tight"><img className ="h-10" src="https://res.cloudinary.com/dy1txrjmy/image/upload/v1751370780/tutorwisespaces_bpbt1o.png" alt="" /></span>
         </div>
       </div>
 
